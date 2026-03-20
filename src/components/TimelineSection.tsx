74da1d3fda0988656598446e7b0bb6f818830dfa
Md. Moinul Hasan Akash <akash.moinulhasan@gmail.com>
2026-03-21 00:34:32 +0600

    Add id attribute to TimelineSection component

--- src/components/TimelineSection.tsx
+++ src/components/TimelineSection.tsx
@@ -29,7 +29,7 @@ const timelineItems = [
 
 const TimelineSection = () => {
   return (
-    <section className="py-24 relative">
+    <section id="journey" className="py-24 relative">
       <div className="container mx-auto px-6">
         <motion.div
           initial={{ opacity: 0, y: 30 }}
